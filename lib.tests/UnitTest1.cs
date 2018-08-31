99f8e12aab8c2c17e0f68bd5d59e4f74eb62fb2c
Manuel Riezebosch <mriezebosch@gmail.com>
2018-08-31 21:08:19 +0200

    refactor: separate clients for vsrm and vsts and request construction in class

--- lib.tests/UnitTest1.cs
+++ lib.tests/UnitTest1.cs
@@ -25,19 +25,21 @@ namespace lib.tests
         public void Test1()
         {
             string organization = "somecompany";
-            var client = new RestClient($"https://{organization}.vsrm.visualstudio.com/{{project}}/_apis/{{area}}/{{resource}}/");
-            
-            var request = new RestRequest(Method.GET);
-            request
-                .AddUrlSegment("project", "SOx-compliant-demo")
-                .AddUrlSegment("area", "release")
-                .AddUrlSegment("resource", "definitions");
+            var client = VsrmClient(organization);
+            var request = Requests.ReleaseDefinitions();
 
             request.AddHeader("authorization", GenerateAuthorizationHeader(token));
-            var definitions = client.Execute(request);
-            
+            var definitions = client.Execute<JsonCollection<ReleaseDefinition>>(request);
+
             definitions.StatusCode.ShouldBe(HttpStatusCode.OK);
+            definitions.Data.Value.ShouldAllBe(_ => !string.IsNullOrEmpty(_.Name));
         }
+
+        private static IRestClient VsrmClient(string organization)
+        {
+            return new RestClient($"https://{organization}.vsrm.visualstudio.com/{{project}}/_apis/{{area}}/{{resource}}/");
+        }
+
 
         class ReleaseDefinition 
         {
@@ -48,19 +50,40 @@ namespace lib.tests
         public void ReleaseDefinitionDetails()
         {
             string organization = "somecompany";
-            var client = new RestClient($"https://{organization}.vsrm.visualstudio.com/{{project}}/_apis/{{area}}/{{resource}}/");
-            
-            var request = new RestRequest("2", Method.GET);
-            request
-                .AddUrlSegment("project", "SOx-compliant-demo")
-                .AddUrlSegment("area", "release")
-                .AddUrlSegment("resource", "definitions");
+            var client = VsrmClient(organization);
 
-            request.AddHeader("authorization", GenerateAuthorizationHeader(token));
+            var request = Requests.ReleaseDefinition("2").AddHeader("authorization", GenerateAuthorizationHeader(token));
             var definition = client.Execute<ReleaseDefinition>(request);
-            
+
             definition.StatusCode.ShouldBe(HttpStatusCode.OK);
             definition.Data.Name.ShouldBe("demo SOx");
+        }
+
+        static class Requests 
+        {
+            public static IRestRequest ReleaseDefinition(string id)
+            {
+                return new RestRequest(id, Method.GET)
+                    .AddUrlSegment("project", "SOx-compliant-demo")
+                    .AddUrlSegment("area", "release")
+                    .AddUrlSegment("resource", "definitions");
+            }
+
+            public static IRestRequest ServiceEndpoints()
+            {
+                return new RestRequest(Method.GET)
+                    .AddUrlSegment("project", "SOx-compliant-demo")
+                    .AddUrlSegment("area", "serviceendpoint")
+                    .AddUrlSegment("resource", "endpoints");
+            }
+
+            public static IRestRequest ReleaseDefinitions()
+            {
+                return new RestRequest(Method.GET)
+                    .AddUrlSegment("project", "SOx-compliant-demo")
+                    .AddUrlSegment("area", "release")
+                    .AddUrlSegment("resource", "definitions");
+            }
         }
 
         class ServiceEndpoint 
@@ -77,19 +100,18 @@ namespace lib.tests
         public void QueryServiceConnections()
         {
             string organization = "somecompany";
-            var client = new RestClient($"https://{organization}.visualstudio.com/{{project}}/_apis/{{area}}/{{resource}}/");
-            
-            var request = new RestRequest(Method.GET);
-            request
-                .AddUrlSegment("project", "SOx-compliant-demo")
-                .AddUrlSegment("area", "serviceendpoint")
-                .AddUrlSegment("resource", "endpoints");
+            var client = VstsClient(organization);
 
-            request.AddHeader("authorization", GenerateAuthorizationHeader(token));
+            var request = Requests.ServiceEndpoints().AddHeader("authorization", GenerateAuthorizationHeader(token));
             var definition = client.Execute<JsonCollection<ServiceEndpoint>>(request);
-            
+
             definition.StatusCode.ShouldBe(HttpStatusCode.OK);
             definition.Data.Value.ShouldContain(e => e.Name == "p02-prd-devautsox-deploy (SPN)");
+        }
+
+        private static IRestClient VstsClient(string organization)
+        {
+            return new RestClient($"https://{organization}.visualstudio.com/{{project}}/_apis/{{area}}/{{resource}}/");
         }
 
         private string GenerateAuthorizationHeader(string token)
