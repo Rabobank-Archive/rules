8e6848f4223abc30b458c00d0db70e7c971db05b
Manuel Riezebosch <mriezebosch@gmail.com>
2018-08-31 15:31:04 +0200

    leeg projectje

---
+++ lib.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace lib.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            new Class1();
+        }
+    }
+}
