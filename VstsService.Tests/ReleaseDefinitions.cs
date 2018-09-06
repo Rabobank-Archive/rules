686f5a2869326064af374254cbc7097a76b99672
Manuel Riezebosch <mriezebosch@gmail.com>
2018-09-06 20:58:34 +0200

    refactor: release test in own file

--- VstsService.Tests/ReleaseDefinitions.cs
+++ VstsService.Tests/ReleaseDefinitions.cs
@@ -1,16 +1,15 @@
 using Xunit;
 using Shouldly;
 using System.Net;
-using System.Linq;
 
 namespace SecurePipelineScan.VstsService.Tests
 {
-    public class Release : IClassFixture<TestConfig>
+    public class ReleaseDefinitions : IClassFixture<TestConfig>
     {
         private readonly TestConfig config;
         private readonly IVstsRestClient client;
 
-        public Release(TestConfig config)
+        public ReleaseDefinitions(TestConfig config)
         {
             this.config = config;
             client = new VstsRestClient(config.Organization, config.Token);
@@ -32,36 +31,6 @@ namespace SecurePipelineScan.VstsService.Tests
 
             definition.StatusCode.ShouldBe(HttpStatusCode.OK);
             definition.Data.Name.ShouldBe("demo SOx");
-        }
-
-        [Fact]
-        public void ReleaseWithApproval()
-        {
-            const string id = "616";
-
-            var release = client.Execute(Requests.Release.Releases("TAS", id));
-            release.ErrorMessage.ShouldBeNull();
-
-            release.StatusCode.ShouldBe(HttpStatusCode.OK);
-            release.Data.Id.ShouldBe(id);
-            release.Data.Environments.ShouldNotBeEmpty();
-
-            var env = release.Data.Environments.First();
-            env.Id.ShouldNotBeNullOrEmpty();
-            env.PreDeployApprovals.ShouldNotBeEmpty();
-            env.DeploySteps.ShouldNotBeEmpty();
-
-            var deploy = env.DeploySteps.First();
-            deploy.RequestedFor.ShouldNotBeNull();
-            deploy.RequestedFor.Id.ShouldNotBeNull();
-            deploy.LastModifiedBy.ShouldNotBeNull();
-
-            var predeploy = env.PreDeployApprovals.First();
-            predeploy.Status.ShouldNotBeNullOrEmpty();
-            predeploy.ApprovalType.ShouldNotBeNullOrEmpty();
-            predeploy.IsAutomated.ShouldBe(false);
-            predeploy.ApprovedBy.ShouldNotBeNull();
-            predeploy.ApprovedBy.DisplayName.ShouldNotBeNullOrEmpty();
         }
     }
 }
