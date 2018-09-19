31530fe6bae85fc1d595b81a7e0f96ed7e38961a
Geert van der Cruijsen <geert.van.der.cruijsen@rabobank.nl>
2018-09-19 14:55:19 +0200

    VSTS (yo yo yo) Service addition for repositories and reviewer policies

---
+++ VstsService/Response/MinimumNumberOfReviewersPolicy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurePipelineScan.VstsService.Response
+{
+    public class MinimumNumberOfReviewersPolicy
+    {
+        public string Id { get; internal set; }
+
+        public bool? IsEnabled { get; set; }
+        public bool? IsBlocking { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public MinimumNumberOfReviewersPolicySettings Settings { get; set; }
+
+    }
+
+    public class MinimumNumberOfReviewersPolicySettings
+    {
+        public int? MinimumApproverCount { get; set; }
+        public bool? CreatorVoteCounts { get; set; }
+        public bool? AllowDownvotes { get; set; }
+        public bool? ResetOnSourcePush { get; set; }
+
+        public List<Scope> Scope { get; set; }
+
+    }
+}
---
+++ VstsService/Response/RequiredReviewerPolicy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurePipelineScan.VstsService.Response
+{
+    public class RequiredReviewersPolicy
+    {
+        public string Id { get; internal set; }
+
+        public bool? IsEnabled { get; set; }
+        public bool? IsBlocking { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public RequiredReviewersPolicySettings Settings { get; set; }
+
+    }
+
+    public class RequiredReviewersPolicySettings
+    {
+        public List<Guid> RequiredReviewerIds { get; set; }
+
+        public List<Scope> Scope { get; set; }
+
+        public List<string> FileNamePatterns { get; set; }
+
+    }
+
+    public class Scope
+    {
+        public string RefName { get; set; }
+        public Guid RepositoryId { get; set; }
+        public string MatchKind { get; set; }
+    }
+}
