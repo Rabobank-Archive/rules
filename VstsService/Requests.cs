7a607b5a81eb83490df0348ba6c8fa66b4367527
Pruyssenaere de la Woestijne de, L (Leon) <Leon.de.Pruyssenaere.de.la.Woestijne@rabobank.nl>
2018-10-22 10:51:11 +0000

    Merged PR 4098: Fix top 100 -> top 1000
    Fix top 100 -> top 1000

--- VstsService/Requests.cs
+++ VstsService/Requests.cs
@@ -25,7 +25,7 @@ namespace SecurePipelineScan.VstsService
 
         public static IVstsRestRequest<Response.Multiple<Response.Project>> Projects()
         {
-            return new VstsRestRequest<Response.Multiple<Response.Project>>($"_apis/projects?`$top=1000&api-version=4.1-preview.2",Method.GET);
+            return new VstsRestRequest<Response.Multiple<Response.Project>>($"_apis/projects?$top=1000&api-version=4.1-preview.2",Method.GET);
         }
 
         public static class ServiceEndpoint
