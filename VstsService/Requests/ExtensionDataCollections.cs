c476a8336809f9d76bbf484bf64032f1247ac65f
Geert van der Cruijsen <geert@vdcruijsen.net>
2019-02-01 16:08:30 +0100

    removed comments and fixed casing of json object

--- VstsService/Requests/ExtensionDataCollections.cs
+++ VstsService/Requests/ExtensionDataCollections.cs
@@ -4,21 +4,6 @@ namespace SecurePipelineScan.VstsService.Requests
 {
     public static class ExtensionDataCollections
     {
-        //GET https://manuel.extmgmt.visualstudio.com/_apis/ExtensionManagement/InstalledExtensions/{publisher}/{extensionName}/Data/Scopes/Default/Current/Collections/GitRepositories/Documents/test
-
-        //PUT https://manuel.extmgmt.visualstudio.com/_apis/ExtensionManagement/InstalledExtensions/riezebosch/my-first-extension/Data/Scopes/Default/Current/Collections/GitRepositories/Documents
-        //Authorization: {{auth
-        //    }
-        //}
-        //Content-Type: application/json
-        //Accept: api-version=3.1-preview.1
-
-        //{
-        //    "id": "test",
-        //    "isCompliant": true,
-        //    "__etag": 3
-        //}
-
         public static IVstsRestRequest<Response.ExtensionCollectionData> ExtensionData(string publisher, string extensionName, string collection, string id)
         {
             return new ExtmgmtRequest<Response.ExtensionCollectionData>($"_apis/ExtensionManagement/InstalledExtensions/{ publisher }/{ extensionName }/Data/Scopes/Default/Current/Collections/{ collection}/Documents/{id}?api-version=3.1-preview.1");
@@ -32,15 +17,15 @@ namespace SecurePipelineScan.VstsService.Requests
 
     public class ExtensionData
     {
-        public string id { get; set; }
-        public object data { get; set; }
+        public string Id { get; set; }
+        public object Data { get; set; }
 
         public int __etag { get { return -1; }}
 
         public ExtensionData(string id, object data)
         {
-            this.id = id;
-            this.data = data;
+            this.Id = id;
+            this.Data = data;
         }
     }
 }
