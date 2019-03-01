5cf9e3d3445c7c18b58c643b9329757ad03e8497
Denny van Kleef <denny.van.kleef@rabobank.nl>
2019-03-01 13:17:10 +0100

    Moved PermissionIdTests to EnumExtensionsTest

--- Common.Tests/EnumExtensionsTest.cs
+++ Common.Tests/EnumExtensionsTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using Common;
 using Xunit;
 
@@ -10,5 +12,16 @@ namespace SecurePipelineScan.Common.Tests
         {
             Assert.Empty(EnumExtensions.GetDisplayName(null));
         }
+
+        [Fact]
+        public void GetDisplayNameWithEnumShouldReturnDisplayName()
+        {
+            Assert.Equal("Not Set", PermissionId.NotSet.GetDisplayName());
+            Assert.Equal("Deny", PermissionId.Deny.GetDisplayName());
+            Assert.Equal("Deny (inherited)", PermissionId.DenyInherited.GetDisplayName());
+            Assert.Equal("Allow", PermissionId.Allow.GetDisplayName());
+            Assert.Equal("Allow (inherited)", PermissionId.AllowInherited.GetDisplayName());
+        }
+
     }
 }
