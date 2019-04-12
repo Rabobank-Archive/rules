755e79ecfb4309b43f85dc0e13af2ad911b5eb7d
Manuel Riezebosch <mriezebosch@gmail.com>
2019-04-12 15:18:35 +0200

    feat(rest): expanded the query of security namespaces

--- VstsService/Response/NamespaceAction.cs
+++ VstsService/Response/NamespaceAction.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace SecurePipelineScan.VstsService.Response
 {
     public class NamespaceAction
     {
         public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public Guid NamespaceId { get; set; }
+        public int Bit { get; set; }
     }
 }
