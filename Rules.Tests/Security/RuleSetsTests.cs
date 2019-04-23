bb44a92a1b78192ba30381191bb676abcfc90a4b
kleefd <2manwypn6swv2stt3ooaxjgx7nvda36j4cfg3htzxrutzbceew3a>
2019-04-23 13:10:00 +0200

    feat(security): Added new NobodyCanDeleteRepository rules to RulesProvider.

--- Rules.Tests/Security/RuleSetsTests.cs
+++ Rules.Tests/Security/RuleSetsTests.cs
@@ -10,10 +10,13 @@ namespace SecurePipelineScan.Rules.Tests.Security
     public class RuleSetsTests
     {
         [Fact]
-        public void GlobalPermissions()
+        public void GetPermissions()
         {
             var rules = new RulesProvider().GlobalPermissions(Substitute.For<IVstsRestClient>());
             rules.OfType<NobodyCanDeleteTheTeamProject>().ShouldNotBeEmpty();
+            
+            var repoRules = new RulesProvider().RepositoryPermissions(Substitute.For<IVstsRestClient>());
+            repoRules.OfType<NobodyCanDeleteTheRepository>().ShouldNotBeEmpty();
         }
     }
 }
--- Rules/Security/RulesProvider.cs
+++ Rules/Security/RulesProvider.cs
@@ -6,6 +6,8 @@ namespace SecurePipelineScan.Rules.Security
     public interface IRulesProvider
     {
         IEnumerable<IProjectRule> GlobalPermissions(IVstsRestClient client);
+        IEnumerable<IRepositoryRule> RepositoryPermissions(IVstsRestClient client);
+        
     }
 
     public class RulesProvider : IRulesProvider
@@ -13,6 +15,12 @@ namespace SecurePipelineScan.Rules.Security
         public IEnumerable<IProjectRule> GlobalPermissions(IVstsRestClient client)
         {
             yield return new NobodyCanDeleteTheTeamProject(client);
+                
+        }
+
+        public IEnumerable<IRepositoryRule> RepositoryPermissions(IVstsRestClient client)
+        {
+            yield return new NobodyCanDeleteTheRepository(client);
         }
     }
 }
