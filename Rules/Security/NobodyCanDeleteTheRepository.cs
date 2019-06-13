61ec7f11f14b8f5d0f7fee2f459de4bda9ccbd98
kleefd <2manwypn6swv2stt3ooaxjgx7nvda36j4cfg3htzxrutzbceew3a>
2019-06-13 12:21:13 +0200

    using PermissionsSetId = SecurePipelineScan.VstsService.Response.PermissionsSetId;

--- Rules/Security/NobodyCanDeleteTheRepository.cs
+++ Rules/Security/NobodyCanDeleteTheRepository.cs
@@ -4,6 +4,7 @@ using SecurePipelineScan.VstsService;
 using SecurePipelineScan.VstsService.Requests;
 using Response = SecurePipelineScan.VstsService.Response;
 using ApplicationGroup = SecurePipelineScan.VstsService.Response.ApplicationGroup;
+using PermissionsSetId = SecurePipelineScan.VstsService.Response.PermissionsSetId;
 
 namespace SecurePipelineScan.Rules.Security
 {
@@ -46,11 +47,11 @@ namespace SecurePipelineScan.Rules.Security
         protected override async Task<IEnumerable<ApplicationGroup>> LoadGroups(string projectId, string id) =>
             (await _client.GetAsync(VstsService.Requests.ApplicationGroup.ExplicitIdentitiesRepos(projectId, NamespaceId))).Identities;
 
-        protected override async Task<Response.PermissionsSetId> LoadPermissionsSetForGroup(string projectId, string id,
+        protected override async Task<PermissionsSetId> LoadPermissionsSetForGroup(string projectId, string id,
             ApplicationGroup @group) =>
             (await _client.GetAsync(Permissions.PermissionsGroupRepository(projectId, NamespaceId, group.TeamFoundationId, id)));
 
-        protected override async Task UpdatePermissionToDeny(string projectId, ApplicationGroup group, Response.PermissionsSetId permissionSetId, Response.Permission permission) =>
+        protected override async Task UpdatePermissionToDeny(string projectId, ApplicationGroup group, PermissionsSetId permissionSetId, Response.Permission permission) =>
             await _client.PostAsync(Permissions.ManagePermissions(projectId), new Permissions.ManagePermissionsData(@group.TeamFoundationId, permissionSetId.DescriptorIdentifier, permissionSetId.DescriptorIdentityType, permission).Wrap());
     }
 }
