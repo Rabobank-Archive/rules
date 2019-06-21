34010eb04fb5423b9b64c63fd15f338f14ff28df
Manuel Riezebosch <mriezebosch@gmail.com>
2019-06-21 14:27:41 +0200

    refactor(multiple): improved continuation logic and simplified building request

--- VstsService/MultipleEnumerator.cs
+++ VstsService/MultipleEnumerator.cs
@@ -9,31 +9,23 @@ namespace SecurePipelineScan.VstsService
 {
     internal class MultipleEnumerator<TResponse> : IEnumerable<TResponse>
     {
-        private readonly IVstsRequest _request;
-        private readonly string _token;
-        private readonly string _organization;
+        private readonly IFlurlRequest _request;
 
         public MultipleEnumerator(IVstsRequest request, string organization, string token)
         {
-            _request = request;
-            _token = token;
-            _organization = organization;
+            _request = new Url(request.BaseUri(organization))
+                .WithBasicAuth(string.Empty, token)
+                .AppendPathSegment(request.Resource)
+                .SetQueryParams(request.QueryParams);
         }
 
         public IEnumerator<TResponse> GetEnumerator()
         {
-            string continuationtoken = null;
-            
-            while(true)
+            var more = true;
+            while(more)
             {
-                var request = new Url(_request.BaseUri(_organization))
-                        .AppendPathSegment(_request.Resource)
-                        .WithBasicAuth(string.Empty, _token)
-                        .SetQueryParams(_request.QueryParams)
-                        .SetQueryParam("continuationToken", continuationtoken);
-                
                 // Need headers & result so capture task first: https://stackoverflow.com/a/53514668/129269
-                var task = request.GetAsync();
+                var task = _request.GetAsync();
 
                 var response = task.ConfigureAwait(false).GetAwaiter().GetResult();
                 var data = task.ReceiveJson<Multiple<TResponse>>().ConfigureAwait(false).GetAwaiter().GetResult();
@@ -43,11 +35,8 @@ namespace SecurePipelineScan.VstsService
                     yield return item;
                 }
 
-                continuationtoken = response.Headers.TryGetValues("x-ms-continuationtoken", out var values) ? values.FirstOrDefault() : null;
-                if (continuationtoken == null)
-                {
-                    break;
-                }
+                more = response.Headers.TryGetValues("x-ms-continuationtoken", out var tokens);                
+                _request.SetQueryParam("continuationToken", tokens?.First());
             } 
         }
 
