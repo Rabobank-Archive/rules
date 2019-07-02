8723affbb893c2f2116274013946591683248a64
kleefd <2manwypn6swv2stt3ooaxjgx7nvda36j4cfg3htzxrutzbceew3a>
2019-07-02 08:51:09 +0200

    Added additional assert on projectName

--- VstsService.Tests/Projects.cs
+++ VstsService.Tests/Projects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using SecurePipelineScan.VstsService.Requests;
 using Shouldly;
 using Xunit;
 
@@ -24,25 +25,24 @@ namespace SecurePipelineScan.VstsService.Tests
         [Fact]
         public void QueryProjects()
         {
-            var definitions = _client.Get(Requests.Project.Projects());
+            var definitions = _client.Get(Project.Projects());
             definitions.ShouldAllBe(_ => !string.IsNullOrEmpty(_.Name));
         }
 
         [Fact]
         public async Task QueryProjectProperties()
         {
-            var projects = _client.Get(Requests.Project.Projects());
+            var projects = _client.Get(Project.Projects());
             var firstProjectName = projects.First().Name;
 
-            var id = await _client.GetAsync(Requests.Project.Properties(firstProjectName));
+            var id = await _client.GetAsync(Project.Properties(firstProjectName));
             id.ShouldNotBeNull();
         }
 
         [Fact]
         public async Task QuerySingleProjectWithNameShouldReturnAProject()
         {
-            
-            var project = await _client.GetAsync(Requests.Project.ProjectByName("TAS"));
+            var project = await _client.GetAsync(Project.ProjectByName("TAS"));
             project.ShouldNotBeNull();
             project.Name.ShouldBe("TAS");
         }
