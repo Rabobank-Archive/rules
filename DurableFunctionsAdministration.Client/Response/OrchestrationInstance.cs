822c8569d19e98debed7ea5e7ce2c9947e16f6c6
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-07-16 16:53:46 +0200

    Create POCO for OrchestrationInstance

--- DurableFunctionsAdministration.Client/Response/OrchestrationInstance.cs
+++ DurableFunctionsAdministration.Client/Response/OrchestrationInstance.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace DurableFunctionsAdministration.Client.Response
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class OrchestrationInstance
     {
-        
+        public string InstanceId { get; set; }
+        public string RuntimeStatus { get; set; }
+        public object CustomStatus { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime LastUpdatedTime { get; set; }
     }
 }
