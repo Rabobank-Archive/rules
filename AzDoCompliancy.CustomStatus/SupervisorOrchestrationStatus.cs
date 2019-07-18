eedf8931b2392be4d203d5a7971dd5ce72e2c65c
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-07-18 14:03:23 +0200

    Add custom status objects in separate project

---
+++ AzDoCompliancy.CustomStatus/SupervisorOrchestrationStatus.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AzDoCompliancy.CustomStatus
+{
+    public class SupervisorOrchestrationStatus : CustomStatus
+    {
+        public int TotalProjectCount { get; set; }
+        public override string TypeId
+        {
+            get => TypeIds.SupervisorOrchestrationStatusId;
+            set { }
+        }
+    }
+}
