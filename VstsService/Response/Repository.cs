65165e96ce382e265a08699762b189b317fa159e
Brouwer, R (Rik) <Rik.Brouwer@rabobank.com>
2019-10-08 16:43:39 +0200

    Added url property to repository

--- VstsService/Response/Repository.cs
+++ VstsService/Response/Repository.cs
@@ -1,12 +1,13 @@
-﻿namespace SecurePipelineScan.VstsService.Response
+﻿using System;
+
+namespace SecurePipelineScan.VstsService.Response
 {
     public class Repository
     {
         public string Name { get; set; }
         public string Id { get; set; }
-
         public string DefaultBranch { get; set; }
-
         public Project Project { get; set; }
+        public Uri Url { get; set; }
     }
 }
