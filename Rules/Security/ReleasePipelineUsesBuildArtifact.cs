b81539550cd19bd7aa95a67289a59f421a6dd3a3
Brouwer, R (Rik) <Rik.Brouwer@rabobank.com>
2019-10-14 18:14:29 +0200

    Sonar fixes

--- Rules/Security/ReleasePipelineUsesBuildArtifact.cs
+++ Rules/Security/ReleasePipelineUsesBuildArtifact.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using SecurePipelineScan.VstsService.Response;
 
@@ -10,12 +8,12 @@ namespace SecurePipelineScan.Rules.Security
     public class ReleasePipelineUsesBuildArtifact : IReleasePipelineRule
     {
         public string Description => "The release pipeline should solely use build artifacts";
-
         public string Why => "To make sure artifacts can't be modified when a build is completed";
-
         public bool IsSox => true;
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task<bool> EvaluateAsync(string projectId, ReleaseDefinition releasePipeline)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             if (releasePipeline == null)
                 throw new ArgumentNullException(nameof(releasePipeline));
