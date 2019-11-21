95cacdfe3ca7ce90c6fdf1acc0f21aac4e9c8bdf
Milan Bloem <milan.bloem@rabobank.nl>
2019-11-21 15:03:55 +0100

    Added created by to release

--- VstsService/Response/Release.cs
+++ VstsService/Response/Release.cs
@@ -10,5 +10,6 @@ namespace SecurePipelineScan.VstsService.Response
         public IEnumerable<Environment> Environments { get; set; }
         public IEnumerable<ArtifactReference> Artifacts { get; set; }
         public IEnumerable<string> Tags { get; set; }
+        public Identity CreatedBy { get; set; }
     }
 }
