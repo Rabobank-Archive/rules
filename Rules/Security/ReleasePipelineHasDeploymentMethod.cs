074b7d020c67f3836192356496e48a3f4a9febee
Peter Dol <Peter.Dol@rabobank.nl>
2020-02-17 15:55:48 +0000

    Merged PR 57040: fix button

--- Rules/Security/ReleasePipelineHasDeploymentMethod.cs
+++ Rules/Security/ReleasePipelineHasDeploymentMethod.cs
@@ -6,7 +6,7 @@ using Response = SecurePipelineScan.VstsService.Response;
 
 namespace SecurePipelineScan.Rules.Security
 {
-    public class ReleasePipelineHasDeploymentMethod : IReleasePipelineRule
+    public class ReleasePipelineHasDeploymentMethod : IReleasePipelineRule, IReconcile
     {
         private readonly IProductionItemsResolver _productionItemsResolver;
 
@@ -18,6 +18,8 @@ namespace SecurePipelineScan.Rules.Security
         [ExcludeFromCodeCoverage] public string Description => "Release pipeline has valid CMDB link";
         [ExcludeFromCodeCoverage] public string Link => "https://confluence.dev.somecompany.nl/x/PqKbD";
 
+        public string[] Impact => new string[] { "In the CMDB the deployment method for the CI is set to Azure DevOps and coupled to this release pipeline" };
+
         public async Task<bool?> EvaluateAsync(string projectId, Response.ReleaseDefinition releasePipeline)
         {
             if (releasePipeline == null)
@@ -25,6 +27,11 @@ namespace SecurePipelineScan.Rules.Security
 
             var stages = await _productionItemsResolver.ResolveAsync(projectId, releasePipeline.Id);
             return stages != null && (stages.Any(s => s == "NON-PROD" || releasePipeline.Environments.Any(e => e.Id == s)));
+        }
+
+        public Task ReconcileAsync(string projectId, string itemId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
